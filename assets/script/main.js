b3fe30e517bfc7d46109c5a2139e66e9d7bccd40
Jimmy_dingo <you@example.com>
2020-04-23 12:04:13 +0300

    Added DOM interaction

--- assets/script/main.js
+++ assets/script/main.js
@@ -15,10 +15,10 @@ function comMeans(computer) {
     if (computer <= 0.33) {
         computer = possibilities[0];
     }
-    else if(computer >= 0.33 && computer <= 0.66){
+    else if(computer > 0.33 && computer <= 0.66){
         computer = possibilities[1];
     }
-    else if(computer >= 0.66 && computer <= 0.99){
+    else if(computer > 0.66 && computer <= 0.99){
         computer = possibilities[2];
     }
     return computer;
@@ -33,61 +33,94 @@ console.log('User choice: ' + comMeans(user)); // here i reused the same functio
 computer = comMeans(computer);
 user = comMeans(user);
 
-var buttonClick = document.querySelector('button');
-buttonClick.addEventListener('click', changeResults);
-
 function play(options, user, computer) {
+    //function variables
+
     options = ['Tie!', 'User wins!', 'Computer wins!'];
+    var computerImg = document.querySelector('#computer-img');
+    var userImg = document.querySelector('#user-img');
+    var userResult = document.querySelector('.user-side p');
+    var computerResult = document.querySelector('.computer-side p');
+    var winnerResult = document.querySelector('.result');
+    
+    //function conditions
+
     if (user === 'Rock' && computer === 'Rock'){
         winner == options[0];
+        userResult.innerText = user;
+        computerResult.innerText = computer;
+        winnerResult.innerText = winner;
         console.log("Tie!");
     }
     else if (user === 'Paper' && computer === 'Paper'){
         winner = options[0];
+        userImg.src="assets/images/paper.png"
+        computerImg.src="assets/images/paper.png"
+        userResult.innerText = user;
+        computerResult.innerText = computer;
+        winnerResult.innerText = winner;
         console.log("Tie!");
     }
     else if (user === 'Scissors' && computer === 'Scissors'){
         winner = options[0];
+        userImg.src="assets/images/scissors.png";
+        computerImg.src="assets/images/scissors.png";
+        userResult.innerText = user;
+        computerResult.innerText = computer;
+        winnerResult.innerText = winner;
         console.log("Tie!");
     }
     else if (user === 'Paper' && computer === 'Rock'){
         winner = options[1];
+        userImg.src="assets/images/paper.png"
+        userResult.innerText = user;
+        computerResult.innerText = computer;
+        winnerResult.innerText = winner;
         console.log("User wins!");
     }
     else if (user === 'Rock' && computer === 'Scissors'){
         winner = options[1];
+        computerImg.src="assets/images/scissors.png";
+        userResult.innerText = user;
+        computerResult.innerText = computer;
+        winnerResult.innerText = winner;
         console.log("User wins!");
     }
     else if (user === 'Scissors' && computer === 'Rock'){
         winner = options[2];
+        userImg.src="assets/images/scissors.png";
+        userResult.innerText = user;
+        computerResult.innerText = computer;
+        winnerResult.innerText = winner;
         console.log("Computer wins!");
     }
     else if (user === 'Rock' && computer === 'Paper'){
         winner = options[2];
+        computerImg.src="assets/images/paper.png"
+        userResult.innerText = user;
+        computerResult.innerText = computer;
+        winnerResult.innerText = winner;
         console.log("Computer wins!");
     }
     else if (user === 'Paper' && computer === 'Scissors'){
         winner = options[2];
+        userImg.src="assets/images/paper.png"
+        computerImg.src="assets/images/scissors.png";
+        userResult.innerText = user;
+        computerResult.innerText = computer;
+        winnerResult.innerText = winner;
         console.log("Computer wins!");
     }
     else if(user === 'Scissors' || computer === 'Paper'){
         winner = options[1];
+        userImg.src="assets/images/scissors.png";
+        computerImg.src="assets/images/paper.png"
+        userResult.innerText = user;
+        computerResult.innerText = computer;
+        winnerResult.innerText = winner;
         console.log("User wins!");
     }
     return winner;
 }
 
 play(winner, user, computer);
-
-// Interaction with the DOM 
-
-function changeResults(){
-    console.log('button was clicked');
-    play(winner, user, computer);
-    var userResult = document.querySelector('.user-side p');
-    var computerResult = document.querySelector('.computer-side p');
-    var winnerResult = document.querySelector('.result');
-    userResult.innerText = user;
-    computerResult.innerText = computer;
-    winnerResult.innerText = winner; 
-}
